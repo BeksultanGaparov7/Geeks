33ba09b2d8e906ae4da2aee87b0dc68a2ddd7d61
user <gaparovbeksultan7@gmail.com>
2024-08-15 21:35:03 +0600

    EASSY

---
+++ main.py
@@ -0,0 +1,60 @@
+class Aplicase:
+    def turn_on(self):
+        pass
+    def turn_off(self):
+        pass
+    def use(self):
+        pass
+    def repair(self):
+        pass
+
+class WashingMachine(Aplicase):
+    def __init__(self):
+        super().__init__()
+
+    def use(self):
+        print('Стиральная машина начала стирку')
+
+    def repair(self):
+        print('Ремонт стиральной машины')
+
+class Microwave(Aplicase):
+    def __init__(self):
+        super().__init__()
+
+    def use(self):
+        print('Микроволновая печь разогревает еду')
+
+    def repair(self):
+        print('Ремонт микроволновой печи')
+
+class Refrigerator(Aplicase):
+    def __init__(self):
+        super().__init__()
+
+    def use(self):
+        print('Холодильник охлаждает продукты')
+
+    def repair(self):
+        print('Ремонт холодильника')
+
+class Message:
+    def print(self):
+        print('Да ПЖ разблокируйте! В ТГ')
+        print('Я больше не буду, не знал я!')
+
+wm = WashingMachine()
+mv = Microwave()
+rf = Refrigerator()
+m = Message()
+
+wm.use()
+wm.repair()
+mv.use()
+mv.repair()
+rf.use()
+rf.repair()
+m.print()
+
+
+
